2361f5964605e26f646f2cf48be90af86aeb3065
jack-king1 <jackk@neonplay.com>
2023-02-06 17:31:26 +0000

    responsive menu responsive

--- brochure-site/src/Components/MainMenu.js
+++ brochure-site/src/Components/MainMenu.js
@@ -10,19 +10,47 @@ function MainMenu(props) {
   const [menuState, setMenuState] = useState(false);
   const [centerMenuButton, setCenterMenuButtons] = useState(false);
 
-  return (
-    <div className="relative text-white">
-      <img src={CoffeeShop} className="w-full object-cover" />
-      <div className="absolute inset-0 flex justify-center">
-        <div className="rounded p-4 shadow-md">
-          <div className="flex flex-wrap justify-center items-center">
-            <div className="md:w-1/4 text-left md:text-center">Option 1</div>
-            <div className="w-2/5 md:w-1/2 text-center">
-              <img src={Logo} alt="Logo" className="px-4" />
-            </div>
-            <div className="md:w-1/4 text-right md:text-center">Option 2</div>
+  const DesktopMenu = () => {
+    return (
+      <div className="rounded p-4">
+        <div className="flex justify-between items-center">
+          <div className=" text-left md:text-center">Order Now</div>
+          <div className=" text-left md:text-center">Locations</div>
+          <div className="w-2/5 md:w-1/2 text-center">
+            <img src={Logo} alt="Logo" className="px-4" />
+          </div>
+          <div className=" text-right md:text-center">About Us</div>
+          <div className=" text-right md:text-center">Gift Cards</div>
+        </div>
+      </div>
+    );
+  };
+
+  const MobileMenu = () => {
+    return (
+      <div>
+        <div className="flex mt-4">
+          <div className="w-3/5 text-center justify-center items-center mx-auto">
+            <img src={Logo} alt="Logo" className="px-4" />
           </div>
         </div>
+        <div className="rounded p-4 mx-auto">
+          <div className="flex justify-between items-center">
+            <div className="text-left md:text-center">Order Now</div>
+            <div className="text-left md:text-center">Locations</div>
+            <div className="text-right md:text-center">About Us</div>
+            <div className="text-right md:text-center">Gift Cards</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="relative text-white">
+      <img src={CoffeeShop} className="w-full h-screen object-cover" />
+      <div className="absolute inset-0 flex justify-center">
+        {props.screenWidthRef[0] > 768 ? DesktopMenu() : MobileMenu()}
       </div>
     </div>
   );
